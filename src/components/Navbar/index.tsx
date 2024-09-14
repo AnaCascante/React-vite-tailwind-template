aa46566ccb311a9ba688964560e61ba9fd7d6c5b
Ana <91411170+AnaCascante@users.noreply.github.com>
2024-09-14 12:34:50 +0200

    adds changes to navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,14 +1,23 @@
 import { NavLink } from 'react-router-dom';
+import { IoPersonCircle } from 'react-icons/io5';
+import { BiSolidHomeAlt2 } from 'react-icons/bi';
+import Logo from '../../assets/logo.png';
 
 const Navbar = () => {
   return (
     <nav className="bg-slate-400 p-4">
       <ul className="flex space-x-4">
         <li>
-          <NavLink to="/" className="text-yellow-200">
-            Home
+          <NavLink to="/" className="border-spacing-20">
+            <img src={Logo} alt="Logo" className="w-20" />
           </NavLink>
         </li>
+        <li>
+          <NavLink to="/" className="text-black">
+            <BiSolidHomeAlt2 />
+          </NavLink>
+        </li>
+        <IoPersonCircle />
         <li>
           <NavLink to="/login" className="text-yellow-200">
             Login
