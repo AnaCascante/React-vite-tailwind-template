972015825d5e3fd63278d183a0413bcc6fb068c5
Ana <91411170+AnaCascante@users.noreply.github.com>
2024-09-14 15:08:30 +0200

    testing AppTest

--- src/components/App/AppTest.tsx
+++ src/components/App/AppTest.tsx
@@ -4,7 +4,7 @@ import HomePage from '../../pages/HomePage';
 import LoginPage from '../../pages/LoginPage';
 import RegisterPage from '../../pages/RegisterPage';
 
-function App() {
+function AppTest() {
   return (
     <Router>
       <Navbar />
