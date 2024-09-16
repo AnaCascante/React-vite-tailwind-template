d1cbea392e6774af7df4a730f701551b3a3f6dd2
Ana <91411170+AnaCascante@users.noreply.github.com>
2024-09-16 08:35:13 +0200

    adds auth to service

--- src/services/Registration.ts
+++ src/services/Registration.ts
@@ -20,3 +20,41 @@ interface LoginData {
   email: string;
   password: string;
 }
+
+export const RegisterUser = async (data: UserData): Promise<void> => {
+  try {
+    const response = await fetch(`${BaseUrl}/auth/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) {
+      throw new Error('Network not responding');
+    }
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const LoginUser = async (data: LoginData): Promise<void> => {
+  try {
+    const response = await fetch(`${BaseUrl}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) {
+      throw new Error('Network not responding');
+    }
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
