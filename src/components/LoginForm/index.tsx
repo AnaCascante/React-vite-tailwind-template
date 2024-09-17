4e07f49e5f2ffe6f09f5b12020f4b117981411da
Ana <91411170+AnaCascante@users.noreply.github.com>
2024-09-17 10:36:32 +0200

    adding login form-not finished

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,1 +1,58 @@
 const API_KEY = import.meta.env.VITE_KEY;
+import { LoginUser } from '../../services/Registration';
+import React, { useState } from 'react';
+
+interface LoginData {
+  email: string;
+  password: string;
+}
+
+const LoginForm: React.FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data: LoginData = {
+      email,
+      password,
+    };
+    try {
+      const response = await LoginUser(data);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="mx-auto flex w-1/2 flex-col space-y-4 p-4"
+    >
+      <label htmlFor="email" className="text-lg font-semibold">
+        Email
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full rounded border border-gray-300 p-2"
+        />
+      </label>
+      <label htmlFor="password" className="text-lg font-semibold">
+        Password
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full rounded border border-gray-300 p-2"
+        />
+      </label>
+      <button type="submit" className="rounded bg-blue-500 p-2 text-white">
+        Login
+      </button>
+    </form>
+  );
+};
