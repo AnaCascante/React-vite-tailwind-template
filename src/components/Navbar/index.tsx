70a515c79441b74e712020914b8b10d336e5cc16
Ana <91411170+AnaCascante@users.noreply.github.com>
2024-09-24 14:48:36 +0200

    fixed path to profile

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,8 +2,12 @@ import { NavLink } from 'react-router-dom';
 import { IoPersonCircle } from 'react-icons/io5';
 import { BiSolidHomeAlt2 } from 'react-icons/bi';
 import Logo from '../../assets/Logo.png';
+import { meLocalStorage } from '../../services/localStorage';
 
 const Navbar = () => {
+  const token = meLocalStorage('token');
+  const isAuth = !!token;
+
   return (
     <nav className="bg-tertiary p-4">
       <ul className="flex flex-wrap items-center justify-between">
@@ -17,25 +21,30 @@ const Navbar = () => {
         {/* Navigation items centered */}
         <div className="flex space-x-4">
           <li>
-            <NavLink to="/" className="text-secondary text-2xl">
+            <NavLink to="/" className="text-2xl text-secondary">
               <BiSolidHomeAlt2 />
             </NavLink>
           </li>
-          <li>
-            <NavLink to="/profile" className="text-secondary text-2xl">
-              <IoPersonCircle />
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/login" className="text-secondary">
-              Login
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/register" className="text-secondary">
-              Register
-            </NavLink>
-          </li>
+          {isAuth ? (
+            <li>
+              <NavLink to="/profile" className="text-2xl text-secondary">
+                <IoPersonCircle />
+              </NavLink>
+            </li>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/login" className="text-secondary">
+                  Login
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/register" className="text-secondary">
+                  Register
+                </NavLink>
+              </li>
+            </>
+          )}
         </div>
       </ul>
     </nav>
